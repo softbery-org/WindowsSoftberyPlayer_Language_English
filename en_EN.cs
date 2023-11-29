b51dd5b15a1c1d18fff86015d4a06ed6d3a0e804
Paweł Tobis - Softbery <softbery@gmail.com>
2023-11-29 10:43:43 +0100

    Updating

---
+++ en_EN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translator;
+
+namespace English
+{
+    public class en_EN : ILanguage
+    {
+        public string Name => "English";
+        public string formSettings_btnSave => "Save";
+
+        public string formSettings_btnCancel => "Cancel";
+
+        public string formSettings_Text => "Options:";
+
+        public string formSettings_labelOptions => "Options:";
+
+        public string videoControlBar_labelSubtilesOnOffText => "Subtiles:";
+
+        public string videoControlBar_labelOn => "On";
+
+        public string videoControlBar_labelOff => "Off";
+        public string videoControlBar_fullscreenOff => "Fullscreen Off";
+        public string videoControlBar_fullscreenOn => "Fullscreen On";
+        public string videoControlBar_startTxt => "Start";
+        public string videoControlBar_subtilesOn => "Subtiles On";
+        public string videoControlBar_subtilesOff => "Subtiles Off";
+    }
+}
